db04566f4c96fe7695fa31e6c4b6faf9a51a2780
Makanov Nurzhan <makanov76@gmail.com>
2024-05-18 00:06:24 +0500

    After adding user

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+
+import './app-info.css'
+function AppInfo() {
+    return (
+        <div className='app-info'>
+            <h1>Employers list</h1>
+            <h2>Total employers list:</h2>
+            <h2>Bonus get:</h2>
+        </div>
+    )
+}
+
+export default AppInfo
---
+++ src/components/search-panel/seacrh-panel.js
@@ -0,0 +1,13 @@
+import './search-panel.css'
+
+function SeacrhPanel() {
+    return (
+        <input
+            type="text"
+            className="form-control search-input"
+            placeholder="Find"
+        />
+    )
+}
+
+export default SeacrhPanel
